92bc42a3f7a5d67af3d0681e634e11f112950f81
Ray Hao <71276800+ray-hao@users.noreply.github.com>
2021-02-05 13:51:27 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export default class applic extends React.Component() {
+    render() {
+        return (
+        <div>
+            <input type="checkbox"></input>
+            <p>Item One</p>
+
+            <input type="checkbox"></input>
+            <p>Item One</p>
+
+            <input type="checkbox"></input>
+            <p>Item One</p>
+
+            <input type="checkbox"></input>
+            <p>Item One</p>
+        </div>
+        )
+        }
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import applic from "./app.js"
+
+ReactDOM.render(<applic />, document.getElementById("root"))
